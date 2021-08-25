83a5bdc66f39304e2b3d9139cfcb6121470d908c
Kwame Agyemang <agyemangyeb@gmailcom>
2021-08-25 18:07:16 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// App is going to be our Parent component
+class App extends React.Component {
+render() {
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App;
